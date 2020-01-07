92dd5a66cad50db515c5a7c57e6176daf55547a0
Michael Zhou <zhoumotongxue008@gmail.com>
2020-01-07 20:30:30 +0800

    Updates

--- precache-manifest.83a36647f9f52035a3f9a004254fe2c4.js
+++ precache-manifest.83a36647f9f52035a3f9a004254fe2c4.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3dbdb7b9dcbae1716f72860126d642a1",
+    "revision": "1c00eefa118ae340ed819b03c36090b6",
     "url": "/celer-web-wallet/index.html"
   },
   {
-    "revision": "055fb40d30deeb47802f",
+    "revision": "a85f4e526bf7fcfdc238",
     "url": "/celer-web-wallet/static/css/main.b100e6da.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/celer-web-wallet/static/js/2.e3b6d402.chunk.js"
   },
   {
-    "revision": "055fb40d30deeb47802f",
-    "url": "/celer-web-wallet/static/js/main.38338e33.chunk.js"
+    "revision": "a85f4e526bf7fcfdc238",
+    "url": "/celer-web-wallet/static/js/main.e90617c7.chunk.js"
   },
   {
     "revision": "7732f1f774639c704f29",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/celer-web-wallet/precache-manifest.8d1e0e5712eab7879c238025fdcb021f.js"
+  "/celer-web-wallet/precache-manifest.83a36647f9f52035a3f9a004254fe2c4.js"
 );
 
 self.addEventListener('message', (event) => {
